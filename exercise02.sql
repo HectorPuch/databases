451377e8876ba756cce75b71c742dd7e3f8a08c0
Hector Puch <hectorpuchortonoves@gmail.com>
2024-10-18 20:06:01 +0200

    exercise02_database

---
+++ exercise02.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE db_taller;
+USE db_taller;
+CREATE TABLE mecanics (
+    dni CHAR(9) PRIMARY KEY,
+    nom VARCHAR(9),
+    edat TINYINT UNSIGNED
+);
+
+CREATE TABLE vehicles (
+    matricula CHAR(7) PRIMARY KEY,
+    propietari VARCHAR(20),
+    marca VARCHAR(20),
+    reparat_per CHAR(9) REFERENCES mecanics(dni)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
+);
