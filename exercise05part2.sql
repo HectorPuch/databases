396196805257f7ffa5aeabfcd29356d4b0f07c70
Hector Puch <hectorpuchortonoves@gmail.com>
2024-10-22 17:40:15 +0200

    exercise05part2.sql

---
+++ exercise05part2.sql
@@ -0,0 +1,21 @@
+SHOW DATABASES;
+
+USE db_bufet_advocats;
+
+SHOW TABLES;
+
+DESCRIBE clients;
+
+DESCRIBE assumptes;
+
+DESCRIBE procuradors;
+
+DROP TABLE procuradors;
+
+DROP TABLE assumptes;
+
+DROP TABLE clients;
+
+SHOW TABLES;
+
+SHOW DATABASES;
